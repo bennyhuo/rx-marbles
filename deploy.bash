3c24ec4057f66c9d4bb72e8312a627d65f2a3f42
Adam Charytoniuk <adam.charytoniuk@gmail.com>
2019-06-23 07:48:57 +0100

    fixes in the deploying config.

--- deploy.bash
+++ deploy.bash
@@ -3,9 +3,8 @@
  [distutils]
  index-servers=pypi
  [pypi]
- repository=https://upload.pypi.org/pypi
  username=${PYPI_USERNAME}
  password=${PYPI_PASSWORD}
  EOF
- python setup.py sdist upload;
+ python3 setup.py sdist upload;
  
