b0bd55795b575285a329350734b70b13316a4af2
Adam Charytoniuk <adam.charytoniuk@gmail.com>
2019-06-23 07:31:02 +0100

    Adding missing dependency info.

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 import setuptools
 
-setuptools.setup(name='rxmarbles',
-      version='0.2',
-      description='Marbles diagram generator',
-      url='https://bitbucket.org/achary/rx-marbles/',
-      author='Adam Charytoniuk',
-      author_email="adam.charytoniuk@gmail.com",
-      keywords = ['marbles', 'rx', 'diagrams','reactive'],
-      license='MIT',
-      packages=setuptools.find_packages(),
-       entry_points={
-          'console_scripts': [
-              'marblesgen = rxmarbles.__main__:main'
-          ]
-      },
-      zip_safe=False)
+setuptools.setup(
+    name='rxmarbles',
+    version='0.2.1',
+    description='Marbles diagram generator',
+    url='https://bitbucket.org/achary/rx-marbles/',
+    author='Adam Charytoniuk',
+    author_email="adam.charytoniuk@gmail.com",
+    keywords=['marbles', 'rx', 'diagrams', 'reactive'],
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyparsing',
+    ],
+    entry_points={
+        'console_scripts': [
+            'marblesgen = rxmarbles.__main__:main'
+        ]
+    },
+    zip_safe=False)
